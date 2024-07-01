686da9b149f92ed1257928b4c25a1c329b0187c0
Roshan Sharma <roshansharma2005345@gmail.com>
2024-07-01 15:51:18 +0530

    liveapp

---
+++ src/Comp/Text.jsx
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+import "../index.css"
+
+
+function Text() {
+
+  return (
+    <>
+
+<div className="Container">
+  <h1 className='head'>Do You Love Me...?</h1>
+  <div className="card-body">
+  <a href="https://66826987c6a649f66557cd2a--adorable-klepon-4d24dd.netlify.app/" target='_blank' className="btn">Yes</a>
+  <a href="https://66826987c6a649f66557cd2a--adorable-klepon-4d24dd.netlify.app/" target='_blank' className="btn">Absolutely Yes</a>
+  </div>
+</div>
+
+<h4 className='headf'>Made By Adarsh </h4>
+
+
+     
+    
+    </>
+  )
+}
+
+export default Text
